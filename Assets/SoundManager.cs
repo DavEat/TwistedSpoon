f64d3176af80b04a2b49b204ea0d24acf258ccde
gysju <gysenjulien@gmail.com>
2016-10-08 22:20:41 +0200

    add assietes and a soundManager

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource audioSource;
+	public List<AudioClip> ambianceRandom = new List<AudioClip>();
+	public float everyXTime = 10.0f;
+
+	private float time = 0.0f;
+
+	void Start () 
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	void Update () 
+	{
+		time += Time.deltaTime;
+		if( audioSource.clip != null && !audioSource.isPlaying && time > everyXTime)
+		{
+			time = 0.0f;
+			audioSource.clip = null;
+		}
+		else if (audioSource.clip == null && time >= everyXTime)
+		{
+			audioSource.clip = 	ambianceRandom [Random.Range (0, ambianceRandom.Count)];
+			audioSource.Play ();
+		}
+	}
+}
