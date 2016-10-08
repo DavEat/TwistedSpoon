17a0df0abb283ae66d2f434d9bfa4f70fda49a8b
GrenadeJane <heilea@hotmail.fr>
2016-10-08 16:06:23 +0200

    start to merge the three scenes / can change the profil of the game with "e" and try now to make work the rotate action when the choice is validated

--- Assets/Scripts/Menu/ButtonMenu.cs
+++ Assets/Scripts/Menu/ButtonMenu.cs
@@ -3,16 +3,46 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class ButtonMenu : MonoBehaviour {
-    public string gTriggerName;
 
+    public enum ESTATE_ACTION
+    {
+        PAUSED,
+        INSTRUCTION,
+        GAME,
+        VALIDATED
+    };
+
+    public ESTATE_ACTION gStateAction;
+
+    string mNameTrigger;
+
+    Scene mScene;
     Button mButton;
     Animator mAnimator;
+
 	// Use this for initialization
 	void Start ()
     {
+        mScene = GameObject.FindObjectOfType<Scene>();
+
         mAnimator = gameObject.transform.parent.transform.parent.GetComponent<Animator>();
         mButton = gameObject.GetComponent<Button>();
-        mButton.onClick.AddListener(LaunchAnimation);
+
+        switch (gStateAction)
+        {
+            case ESTATE_ACTION.VALIDATED:
+                {
+                    mButton.onClick.AddListener(StartRotationBoard);
+                }
+                break;
+            default:
+                {
+                    mNameTrigger = gStateAction.ToString();
+                    mButton.onClick.AddListener(LaunchAnimation);
+                }
+                break;
+
+        }
     }
 	
 	// Update is called once per frame
@@ -23,7 +53,11 @@ public class ButtonMenu : MonoBehaviour {
 
     void LaunchAnimation()
     {
-            print(" change level ");
-        mAnimator.SetTrigger(gTriggerName);
+        mAnimator.SetTrigger(mNameTrigger);
+    }
+
+    void StartRotationBoard()
+    {
+        mScene.StartRotateBoard();
     }
 }
---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene : MonoBehaviour {
+
+    // At the first level no lever arm
+    // This 4 levels 
+    // and the same Scene ever 
+
+    int mCurrentLevel = 0;
+
+    Board 
+        mBoard; 
+    CircleBoard 
+        mCircleBoard;
+
+    GameObject
+        mBoardParent,
+        mCircleBoardParent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mBoard = GameObject.FindObjectOfType<Board>();
+        mCircleBoard = GameObject.FindObjectOfType<CircleBoard>();
+
+        mBoardParent = FindParentWithTag(mBoard.transform, "Board").gameObject;
+        mCircleBoardParent = FindParentWithTag(mCircleBoard.transform, "Board").gameObject;
+
+        Init();
+    }
+
+    Transform FindParentWithTag( Transform t_transform , string s_name )
+    {
+        Transform tReturn;
+
+        tReturn = t_transform;
+
+        while ( tReturn != null )
+        { 
+            if (tReturn.tag == s_name)
+            {
+                break;
+            }
+            else
+            {
+                tReturn = tReturn.parent;
+            }
+        }
+
+        return tReturn;
+
+    }
+
+    void Init()
+    {
+        switch (mCurrentLevel)
+        {
+            case 0:
+                {
+                    mBoardParent.gameObject.SetActive(false);
+                    mCircleBoardParent.SetActive(true);
+
+                    mBoard.LeverArm = false;
+                }
+                break;
+            case 1:
+                {
+                    mBoard.LeverArm = true;
+                }
+                break;
+            case 2:
+                {
+                    mBoardParent.gameObject.SetActive( false );
+                    mCircleBoardParent.gameObject.SetActive(true);
+
+                    mCircleBoard.LeverArm = false;
+                }
+                break;
+            case 3:
+                {
+                    mCircleBoard.LeverArm = true;
+                }
+                break;
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            ChangeLevel();
+        }
+	}
+
+    void ChangeLevel()
+    {
+        mCurrentLevel++;
+        Init();
+    }
+
+    public void StartRotateBoard()
+    {
+        /*if (mCurrentLevel >= 0)
+        {
+            mBoard.BoardState = Board.State.State_Rotate;
+        }
+        else if (mCurrentLevel > 1)*/
+        {
+            mCircleBoard.BoardState = CircleBoard.State.State_Rotate;
+        }
+    }
+}
