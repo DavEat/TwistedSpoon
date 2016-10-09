b1cb8b34491ea4786b813dd0315dcc7818827cd2
gysju <gysenjulien@gmail.com>
2016-10-09 11:27:50 +0200

    fix sounds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,6 +41,7 @@ public class GameManager : MonoBehaviour {
 		if(!GameIsLaunched)
 		{
 			GameIsLaunched = true;
+			Amb.Instance.PlayAmbianteSound ();
 			SwitchState(GameState.GameState_IATurn);
 		}
 	}
