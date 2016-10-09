762202fa431ff6c6786396ffd7a3f548d27ad2cc
DavEat <oned@hotmail.be>
2016-10-09 13:33:09 +0200

    sound

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -8,7 +8,7 @@ public class SoundManager : MonoBehaviour {
 	public static SoundManager Instance { get; private set;}
 	public AudioSource audioSource;
 	public List<AudioClip> ambianceRandom = new List<AudioClip>();
-	public List<AudioClip> collisionSound = new List<AudioClip>();
+    public List<AudioClip> collisionSound = new List<AudioClip>();
 
 	public float everyXTime = 10.0f;
 
@@ -41,17 +41,48 @@ public class SoundManager : MonoBehaviour {
 		}
 	}
 
-	public AudioClip PlayCollisionSound ( string name )
+	public AudioClip PlayCollisionSound (string name, bool metal)
 	{
 		AudioClip sound = null;
 		foreach(AudioClip clip in collisionSound)
 		{
-			if(clip.name == name)
-			{
-				return clip;
-			}
-		}
-		return sound;
+            if (name == "Bouteil_A_01" || name == "Bouteil_A_02")
+            {
+                if (metal)
+                    sound = collisionSound[0];
+                else sound = collisionSound[1];
+            }
+            else if (name == "Lingo_01")
+            {
+                if (metal)
+                    sound = collisionSound[2];
+                else sound = collisionSound[3];
+            }
+            else if (name == "Piece_A" || name == "Piece_B" || name == "Piece_C")
+            {
+                if (metal)
+                    sound = collisionSound[4];
+                else sound = collisionSound[5];
+            }
+            else if (name == "Tonneau")
+            {
+                if (metal)
+                    sound = collisionSound[6];
+                else sound = collisionSound[7];
+            }
+            else if (name == "Collier")
+            {
+                if (metal)
+                    sound = collisionSound[8];
+                else sound = collisionSound[9];
+            }
+            else Debug.Log("Error : name of sound not good");
+
+
+        }
+        if (sound == null)
+            sound = collisionSound[4];
+        return sound;
 	}
 
 }
